2f74d183593a11cea4cca4848831cedd1fa13a98
Paul Ebreo <paulebreo@triton.edu>
2018-09-24 15:46:31 -0500

    add schema

--- models.js
+++ models.js
@@ -1,3 +1,39 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr')
+
+const Gardener = db.define('gardener', {
+    name: {
+      type: Sequelize.STRING,
+    },
+    age: {
+      type: Sequelize.INTEGER,
+    }
+});
+
+const Plot = db.define('plot', {
+    shaded: {
+      type: Sequelize.BOOLEAN,
+    },
+    size: {
+      type: Sequelize.INTEGER,
+    }
+});
+
+const Vegetable = db.define('vegetable', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    planted_on: {
+      type: Sequelize.DATE,
+    }
+});
+  
+  
+
 module.exports = db
+
